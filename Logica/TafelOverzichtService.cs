5ebe4ffc3fcdf7d4e9ff15492f479c7178d4d985
royfi <royfillekes@hotmail.com>
2017-01-10 15:30:31 +0100

    d

--- Logica/TafelOverzichtService.cs
+++ Logica/TafelOverzichtService.cs
@@ -22,7 +22,8 @@ namespace Logica
 
                 if (tafelDAO.update = false)
                     return false;
-                
+
+                return true;
             }
             catch
             {
