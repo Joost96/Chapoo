9477b8a076114c1d3d2959fe5f2dfbae0b24140e
Joost96 <joost-k@live.nl>
2017-01-10 11:49:16 +0100

    created Werknemer model

---
+++ Model/Werknemer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Werknemer
+    {
+        private int id;
+
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public string Volledigenaam { get { return Voornaam + " " + Achternaam; } }
+        public string Gebruikersnaam { get; set; }
+        public string Wachtwoord { get; set; }
+        public int Id { get { return id; } }
+
+        public Werknemer(int id , string voornaam, string achternaam, string gebruikersnaam, string wachtwoord)
+        {
+            this.id = id;
+            Voornaam = voornaam;
+            Achternaam = achternaam;
+            Gebruikersnaam = gebruikersnaam;
+            Wachtwoord = wachtwoord;
+        }
+        public override string ToString()
+        {
+            return Volledigenaam;
+        }
+    }
+}
