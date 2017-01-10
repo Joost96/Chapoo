6a02ef00ddbb4f1d676bb5d31c2a341a54740ada
mark-eerstift <m4rkneut@gmail.com>
2017-01-10 16:55:27 +0100

    Started work on BestellingDAO and Product

---
+++ DAL/BestellingDAO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Model;
+
+namespace DAL
+{
+    public class BestellingDAO
+    {
+        public static void Create()
+
+        {
+            SqlConnection conn = Connection.GetConnection("naam");
+            conn.Open();
+            string sql = "INSERT INTO [t_status], [zitplaatsen], [tafel_nr]" +
+                "FROM [RBS_1617F_db01].[dbo].[WERKNEMER] ";
+            SqlCommand command = new SqlCommand(sql, conn);
+
+
+            command.Prepare();
+            command.ExecuteNonQuery();
+        }
+
+
+    }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Product
+    {
+        private int id;
+        public string naam { get; set; }
+        public string omschrijving { get; set; }
+        public int prijs { get; set; }
+        public int voorraad { get; set; }
+
+        public Product (int id, string naam, string omschrijving, int prijs, int voorraad)
+        {
+            int Id = id;
+            string Naam = naam;
+            string Omschrijving = omschrijving;
+            int Prijs = prijs;
+        }
+
+    }
+}
