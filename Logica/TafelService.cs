1e245c9dd5eb60726412686aa8533d1ea02ad368
royfi <royfillekes@hotmail.com>
2017-01-11 15:53:34 +0100

    5

---
+++ Logica/TafelService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using DAL;
+
+namespace Logica
+{
+    public class TafelService
+    {
+        public List<BestellingProduct> getBestellingProducten(int bestellingId)
+        {
+            List<BestellingProduct> bestelling = BestellingProductDAO.ReadAllFromBestellingProducten(bestellingId);
+
+            return bestelling;
+
+        }
+    }
+}
+
