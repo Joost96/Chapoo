0eda7a713d9ea348e4a0c4849f69f9f292b508f8
mark-eerstift <m4rkneut@gmail.com>
2017-01-12 12:45:20 +0100

    GetPrijzen gefixed

--- Logica/TafelService.cs
+++ Logica/TafelService.cs
@@ -17,6 +17,52 @@ namespace Logica
             return bestelling;
 
         }
+        //Gemaakt door mark
+        public struct Prijzen
+        {
+            public double productprijs;
+            public string categorie;
+            public double btwValue;
+            public double productMetBTW;
+        }
+        //gemaakt door mark
+        public Prijzen GetPrijzen(int bestellingId)
+        {
+            //Nieuw prijs struct en lijst bestellingproduct
+            Prijzen prijs = new Prijzen();
+            List<BestellingProduct> bestelling = getBestellingProducten(bestellingId);
+
+            //Voor ieder product in de lijst van de bestelling
+            foreach (Product P in bestelling)
+            {
+                prijs.productprijs = P.Prijs;
+                prijs.categorie = P.Category;
+                
+                //Als de categorie binnen deze waardes valt is hij 6, of 12 procent.
+                int numCat = Int32.Parse(prijs.categorie);
+                if(numCat <= 8 || numCat == 12)
+                {
+                    double btw = 1.06;
+                    prijs.productMetBTW = (prijs.productprijs * btw) ;
+                    prijs.btwValue = (prijs.productMetBTW - prijs.productprijs);
+                }
+                else if (numCat >= 9 && numCat <= 11 )
+                {
+                    double btw = 1.21;
+                    prijs.productMetBTW = (prijs.productprijs * btw);
+                }
+
+
+
+                
+
+                
+            }
+            //Geeft een struct terug met daarin de standaard productprijs, btw waarde per product en de Totaalprijs per product.
+            return prijs;
+
+        }
+
     }
 }
 
