94b6c117f4380b01b1b15ec2cdfa850d12e0d668
royfi <568301@student.inholland.nl>
2017-01-16 15:06:33 +0100

    Finished WerknemerDAO

--- DAL/WerknemerDAO.cs
+++ DAL/WerknemerDAO.cs
@@ -30,10 +30,24 @@ namespace DAL
 
         }
            
-        //public static Delete()
-        //{
-            
-        //}
+       public void Delete(int werknemerId)
+       {
+           SqlConnection conn = Connection.GetConnection("naam");
+           conn.Open();
+
+           string sql = "DELETE " +
+               "FROM [RBS_1617F_db01].[dbo].[WERKNEMER]" +
+               "WHERE [w_nr] = @ParamId";
+
+           SqlCommand cmd = new SqlCommand(sql, conn);
+           
+           cmd.Parameters.Add("@ParamId", System.Data.SqlDbType.Int).Value = werknemerId;
+           
+           cmd.Prepare();
+           cmd.ExecuteNonQuery();
+           
+           conn.Close();
+       }
             
         public Model.Werknemer GetWerknemer(string username)
         {
@@ -71,30 +85,79 @@ namespace DAL
             conn.Close();
             return werknemer;
 
-            
-            
-
-
-
         }
-        //public static GetNextIncrement()
-        //{
+           
+        public List<Werknemer> ReadAll()
+        {
+            List<Werknemer> werknemers = new List<Werknemer>();
+            
 
-        //}
+        
+            SqlConnection conn = Connection.GetConnection("naam");
+            conn.Open();
+            string sql = "SELECT * " +
+                "FROM [RBS_1617F_db01].[dbo].[WERKNEMER] " 
+               
+                ;
+                
+            SqlCommand cmd = new SqlCommand(sql, conn);
 
-        //public static Read()
-        //{
+            cmd.Prepare();
 
-        //}
-        //public static ReadAll()
-        //{
 
-        //}
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                int Id = reader.GetInt32(0);
+                string Voornaam = reader.GetString(1);
+                string Achternaam = reader.GetString(2);
+                WerknemerRol rol = (WerknemerRol)reader.GetInt32(3);
+                string wachtwoord = reader.GetString(4);
+                string gebruikersnaam = reader.GetString(5);
 
-        //public static Update()
-        //{
+                werknemers.Add(new Werknemer(Id, Voornaam, Achternaam, gebruikersnaam, wachtwoord, rol));
 
-        //}
+            }
+
+            conn.Close();
+            return werknemers;
+
+            
+            
+        }
+
+        public void Update(Werknemer werkNemer)
+        {
+            SqlConnection conn = new SqlConnection("naam");
+            int id = werkNemer.Id;
+            string voornaam = werkNemer.Voornaam;
+            string achternaam = werkNemer.Achternaam;
+            string gebruikersNaam = werkNemer.Gebruikersnaam;
+            string w8woord = werkNemer.Wachtwoord;
+            int rol = (int)werkNemer.rol;
+
+            conn.Open();
+
+            string sql = "INSERT INTO [RBS_1617F_db01].[dbo].[WERKNEMER]" +
+                "VALUES(@ParamId, @Voornaam,@Achternaam, @gebruikersNaam @w8woord @rol)" +
+                "WHERE [w_nr] = @ParamId"
+                ;
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            cmd.Parameters.Add("@ParamId", System.Data.SqlDbType.Int).Value = id;
+            cmd.Parameters.Add("@Voornaam", System.Data.SqlDbType.VarChar, 45).Value = voornaam;
+            cmd.Parameters.Add("@Achternaam", System.Data.SqlDbType.VarChar, 45).Value = achternaam;
+            cmd.Parameters.Add("@gebruikersNaam", System.Data.SqlDbType.VarChar,45).Value = gebruikersNaam;
+            cmd.Parameters.Add("@w8woord",System.Data.SqlDbType.VarChar,45).Value = w8woord ;
+            cmd.Parameters.Add("@rol", System.Data.SqlDbType.Int).Value = rol ;
+
+            cmd.Prepare();
+            cmd.ExecuteNonQuery(); ;
+            
+
+            conn.Close();
+        }
       
 
 
