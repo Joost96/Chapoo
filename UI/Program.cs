dc20ceb8584c39c0ee2f68e1ad19988f39a5c454
mirasmedia <575829@student.inholland.nl>
2017-01-18 14:46:54 +0100

    u

--- UI/Program.cs
+++ UI/Program.cs
@@ -16,8 +16,8 @@ namespace UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new BestellingMenuForm(new LoginForm(),new Model.Werknemer(1,"","","","",Model.WerknemerRol.bediening),2));
-            Application.Run(new BetalenForm(new LoginForm(), new Model.Werknemer(1, "", "", "", "", Model.WerknemerRol.bediening), 1));
+            Application.Run(new BestellingMenuForm(new Model.Werknemer(1,"","","","",Model.WerknemerRol.bediening),2));
+            // Application.Run(new KeukenBarForm(new Model.Werknemer(2, "", "", "", "", Model.WerknemerRol.bar),false));
         }
     }
 }
