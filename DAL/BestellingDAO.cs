fbd7973c21a2c970afcc2fd6ceb13c20518ecb2e
mark-eerstift <m4rkneut@gmail.com>
2017-01-18 13:20:20 +0100

    fix decimal

--- DAL/BestellingDAO.cs
+++ DAL/BestellingDAO.cs
@@ -147,7 +147,7 @@ namespace DAL
             SqlCommand command = new SqlCommand(sql, conn);
             command.Parameters.Add("@tafelId", System.Data.SqlDbType.Int).Value = tafelId;
             command.Parameters.Add("@betaalmethode", System.Data.SqlDbType.Int).Value = betaalmethode;
-            command.Parameters.Add("@fooi", System.Data.SqlDbType.Decimal).Value = fooi;
+            command.Parameters.Add("@fooi", System.Data.SqlDbType.Decimal).Value = (Decimal) fooi;
             command.Parameters.Add("@totaalbedrag", System.Data.SqlDbType.Decimal).Value = totaalbedrag;
             command.Parameters.Add("@commentaar", System.Data.SqlDbType.VarChar).Value = commentaar;
             command.Prepare();
