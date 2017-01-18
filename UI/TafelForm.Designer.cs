4b2ba0cc166a9fccecbe493a99083075d5797624
royfi <568301@student.inholland.nl>
2017-01-18 16:56:26 +0100

    Update

--- UI/TafelForm.Designer.cs
+++ UI/TafelForm.Designer.cs
@@ -37,6 +37,7 @@
             this.betalen_btn = new System.Windows.Forms.Button();
             this.bestellen_btn = new System.Windows.Forms.Button();
             this.serveer_btn = new System.Windows.Forms.Button();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // listview_bestelling
@@ -45,7 +46,8 @@
             this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3,
-            this.columnHeader4});
+            this.columnHeader4,
+            this.columnHeader5});
             this.listview_bestelling.FullRowSelect = true;
             this.listview_bestelling.GridLines = true;
             this.listview_bestelling.Location = new System.Drawing.Point(15, 12);
@@ -68,7 +70,7 @@
             // columnHeader3
             // 
             this.columnHeader3.Text = "Commentaar";
-            this.columnHeader3.Width = 167;
+            this.columnHeader3.Width = 126;
             // 
             // columnHeader4
             // 
@@ -81,7 +83,7 @@
             this.betalen_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.betalen_btn.Image = global::UI.Properties.Resources.Shopping_cart;
             this.betalen_btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.betalen_btn.Location = new System.Drawing.Point(269, 425);
+            this.betalen_btn.Location = new System.Drawing.Point(269, 405);
             this.betalen_btn.Name = "betalen_btn";
             this.betalen_btn.Size = new System.Drawing.Size(87, 36);
             this.betalen_btn.TabIndex = 9;
@@ -96,7 +98,7 @@
             this.bestellen_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bestellen_btn.Image = global::UI.Properties.Resources.Save;
             this.bestellen_btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.bestellen_btn.Location = new System.Drawing.Point(15, 425);
+            this.bestellen_btn.Location = new System.Drawing.Point(15, 405);
             this.bestellen_btn.Name = "bestellen_btn";
             this.bestellen_btn.Size = new System.Drawing.Size(87, 36);
             this.bestellen_btn.TabIndex = 8;
@@ -111,7 +113,7 @@
             this.serveer_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.serveer_btn.Image = ((System.Drawing.Image)(resources.GetObject("serveer_btn.Image")));
             this.serveer_btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.serveer_btn.Location = new System.Drawing.Point(142, 425);
+            this.serveer_btn.Location = new System.Drawing.Point(138, 405);
             this.serveer_btn.Name = "serveer_btn";
             this.serveer_btn.Size = new System.Drawing.Size(87, 36);
             this.serveer_btn.TabIndex = 11;
@@ -119,6 +121,11 @@
             this.serveer_btn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.serveer_btn.UseVisualStyleBackColor = false;
             this.serveer_btn.Click += new System.EventHandler(this.serveer_btn_Click);
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Status";
+            this.columnHeader5.Width = 42;
             // 
             // TafelForm
             // 
@@ -129,7 +136,7 @@
             this.Controls.Add(this.betalen_btn);
             this.Controls.Add(this.bestellen_btn);
             this.Name = "TafelForm";
-            this.Text = "Chapoo 16:29";
+            this.Text = "Chapoo 16:56";
             this.Load += new System.EventHandler(this.TafelForm_Load);
             this.ResumeLayout(false);
 
@@ -145,6 +152,7 @@
         private System.Windows.Forms.ColumnHeader columnHeader4;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.Button serveer_btn;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
 
     }
 }
